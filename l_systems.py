f6b84ddd98ae8b422867b9edcc1b6f4c51e9c717
carlescn <100298997+carlescn@users.noreply.github.com>
2023-02-13 12:02:30 +0100

    Small optimization.

--- l_systems.py
+++ l_systems.py
@@ -12,18 +12,20 @@ import numpy as np
 
 SCREEN_WIDTH  = 1080
 SCREEN_HEIGHT = 720
+ROTATE_90 = np.deg2rad(90)
 
 
 class Plant():
-    def __init__(self):
+    def __init__(self, angle, length, width, leaves_size):
         self.array = "X"
         self.rules = {
             "X": "F-[[X]+X]+F[+FX]-X",
             "F": "FF"
         }
-        self.angle  = 25
-        self.length = 5
-        self.width = 3
+        self.angle = np.deg2rad(angle)
+        self.length = length
+        self.width = width
+        self.leaves_size = leaves_size
         self.stem_color  = pygame.Color("burlywood4")
         self.leave_color = pygame.Color("springgreen4")
         self.position = (SCREEN_WIDTH / 3, SCREEN_HEIGHT)
@@ -35,67 +37,77 @@ class Plant():
         self.array = str(new_array)
 
     def draw(self):
-        xys    = [self.position,]
-        angles = [self.angle,]
+        nodes = [{
+            "x": self.position[0],
+            "y": self.position[1],
+            "angle": self.angle,
+        },]
         for letter in self.array:
+            node = nodes[-1]
             match letter:
                 case "F":
-                    # pylint:disable=invalid-name  # single letter x, y
-                    x = xys[-1][0] \
-                        - self.length * np.cos(np.deg2rad(angles[-1] + 90)) \
-                        + np.random.normal(0,0.03)
-                    y = xys[-1][1] \
-                        - self.length * np.sin(np.deg2rad(angles[-1] + 90)) \
-                        + np.random.normal(0,0.08)
-                    line_end = (x, y)
-                    pygame.draw.line(SCREEN, self.stem_color, xys[-1], line_end, self.width)
-                    xys[-1] = line_end
+                    theta = node["angle"] + ROTATE_90
+                    line_start = (node["x"], node["y"])
+                    line_end = (node["x"] - self.length * np.cos(theta) + np.random.normal(0,0.03),
+                                node["y"] - self.length * np.sin(theta) + np.random.normal(0,0.03))
+                    pygame.draw.line(SCREEN, self.stem_color, line_start, line_end, self.width)
+                    node["x"], node["y"] = line_end
 
                 case "+":
-                    angles[-1] += self.angle
+                    node["angle"] += self.angle
 
                 case "-":
-                    angles[-1] -= self.angle
+                    node["angle"] -= self.angle
 
                 case "[":
-                    xys    += [xys[-1],]
-                    angles += [angles[-1],]
+                    nodes.append(node.copy())
 
                 case "]":
-                    pygame.draw.circle(SCREEN, self.leave_color, xys[-1], 4)
-                    xys.pop(-1)
-                    angles.pop(-1)
+                    position = (node["x"], node["y"])
+                    pygame.draw.circle(SCREEN, self.leave_color, position, self.leaves_size)
+                    nodes.pop(-1)
 
+
+def quit_game():
+    pygame.quit()
+    sys.exit()
+
+def print_fps():
+    text = FONT.render(f"FPS: {int(CLOCK.get_fps())}", False, pygame.Color("black"))
+    SCREEN.blit(text, (0,0))
 
 async def main():
     """ Handle the game loop. """
+    plant = Plant(25, 3, 3, 3)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                quit_game()
             if event.type == pygame.KEYDOWN:
-                plant.grow()
+                if event.key == pygame.K_q:
+                    quit_game()
+                else:
+                    plant.grow()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 plant.grow()
             if event.type == TIMER:
                 SCREEN.fill(pygame.Color("cadetblue1"))
                 plant.draw()
 
+        print_fps()
         pygame.display.flip()
+        CLOCK.tick(60)
         await asyncio.sleep(0)
 
 
 if __name__=="__main__":
     pygame.init()
-
     pygame.display.set_caption("Snake")
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    TIMER = pygame.USEREVENT
+    FONT   = pygame.font.SysFont(None, 32)
+    CLOCK  = pygame.time.Clock()
+    TIMER  = pygame.USEREVENT
     pygame.time.set_timer(TIMER, 100)
-
-    plant = Plant()
 
     asyncio.run(main())
     
